dd53dca236d5dd916fb322be88860c170c4fde6a
Mynk-9 <Mynk-9@users.noreply.github.com>
2020-12-20 01:09:31 +0530

    Initial work for the bot
    Initial work done for the bot. It includes:
    1. Added basic methods like pause, appendEmoji etc.
    2. Added main function which runs at script start.

--- index.js
+++ index.js
@@ -12,4 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var reverseMap = require('./ReverseMapper')
+var emojis = require('emojilib').lib;
+var reverseMap = require('./ReverseMapper');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+function pause(millisecs) {
+    return new Promise(resolve => setTimeout(resolve, millisecs));
+}
+function getRandomProperty(obj) {
+    var keys = Object.keys(obj);
+    return obj[keys[keys.length * Math.random() << 0]];
+}
+function appendEmoji(word) {
+    var emojiNameList = reverseMap[word];
+    if (emojiNameList == undefined)
+        return word;
+    else
+        return (word + ' ' + emojis[getRandomProperty(reverseMap[word])]['char']);
+}
+
+(async function main() {
+    var driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+
+
+        await driver.get("https://web.whatsapp.com");
+        await pause(10000);
+
+    } catch (e) {
+        console.log("Error: ");
+        console.log(e);
+    } finally {
+        console.log("Quitting now")
+        driver.quit();
+    }
+})();
